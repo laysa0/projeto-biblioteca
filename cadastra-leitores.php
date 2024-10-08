f769c745bdf3ffdbddc1653285005ed13a4a83fc
Láysa Beatriz <168122343+laysa0@users.noreply.github.com>
2024-10-08 18:28:24 -0300

    Add files via upload

--- cadastra-leitores.php
+++ cadastra-leitores.php
@@ -5,15 +5,35 @@ if (!$conn){
 }
 
 //recebe dados do formulário
-$nome = '';
-$DtNasc = '';
-$celular = '';
-$email=  '';
-$RA = '';
-$endereco = '';
-$NumEnd = '';
-$bairro = '';
-$cidadeUF = '';
+$nome = 'Ryan felipe';
+$dtnasc = '2002-06-27';
+$celular = '(81) 98765-0000';
+$email=  'ryanfelipe@gmail.com';
+$ra = '12155';
+$endereco = 'rua carpina';
+$numend = '55';
+$bairro = 'janga';
+$cidadeuf = 'paulista-PE';
 
 //cria variável para inserir o registro
-$sql = "";
+$sql = "INSERT INTO leitores
+(Nome, DtNasc, Celular, Email, RA, Endereco, NumEnd, Bairro, CidadeUF) values
+('$nome', '$dtnasc', '$celular', '$email', '$ra', '$endereco', $numend', '$bairro', '$cidadeuf')";
+
+//executa a consulta SQL. Se falha, exibe o ero do banco de dados 
+$query = mysqli_query(mysql: $conn,query: $sql) or
+die(mysqli_error(mysql: $conn));
+
+if($query){
+    echo"<center>";
+    echo "cadastro realizado com sucesso!!<br>";
+    echo "<a href='index.php'><button title= 'Homepage'>voltar</button></a>";
+    echo "</center>";
+} else{
+    echo "<center>";
+    echo "Erro ao cadastrar!!<br>";
+    echo "<a href='index.php'><button title='Home page'>voltar</button></a>";
+    echo "</center>";
+}
+?>
+ 
